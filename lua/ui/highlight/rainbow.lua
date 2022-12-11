2f405b50f1b17e21ed8dad5f578fa8c691c648ce
gsaple <yymelau@gmail.com>
2022-12-11 20:44:06 +1100

    bold brackets when toggle for the second time

--- lua/ui/highlight/rainbow.lua
+++ lua/ui/highlight/rainbow.lua
@@ -1,13 +1,13 @@
 M = {}
 M.highlight = function(C, D)
   return {
-    rainbowcol1 = { bg = C.none , fg = C.orange },
-    rainbowcol2 = { bg = C.none , fg = C.blue },
-    rainbowcol3 = { bg = C.none , fg = C.magenta },
-    rainbowcol4 = { bg = C.none , fg = C.yellow },
-    rainbowcol5 = { bg = C.none , fg = C.cyan },
-    rainbowcol6 = { bg = C.none , fg = C.red },
-    rainbowcol7 = { bg = C.none , fg = C.green },
+    rainbowcol1 = { bg = C.none , fg = C.orange, bold = true },
+    rainbowcol2 = { bg = C.none , fg = C.blue, bold = true },
+    rainbowcol3 = { bg = C.none , fg = C.magenta, bold = true },
+    rainbowcol4 = { bg = C.none , fg = C.yellow, bold = true },
+    rainbowcol5 = { bg = C.none , fg = C.cyan, bold = true },
+    rainbowcol6 = { bg = C.none , fg = C.red, bold = true },
+    rainbowcol7 = { bg = C.none , fg = C.green, bold = true },
   }
 end
 return M
