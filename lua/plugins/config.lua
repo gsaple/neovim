1bb2e3fa1d074a799d9da46ab4f469da6d98151d
gsaple <yymelau@gmail.com>
2022-12-21 00:39:28 +1100

    whichkey column max width

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -309,13 +309,13 @@ M.which_key = function()
     ignore_missing = true,
     layout = {
       height = { min = 4, max = 10 }, -- min and max height of the columns
-      width = { min = 20, max = 50 }, -- min and max width of the columns
+      width = { min = 20, max = 90 }, -- min and max width of the columns
       spacing = 10, -- spacing between columns
       align = "left", -- align columns left, center or right
     },
     disable = {
       buftypes = {"prompt"},
-      filetypes = { "TelescopePrompt", "NvimTree"},
+      filetypes = { "TelescopePrompt", "NvimTree" },
     },
     show_help = false,
     show_keys = false,
