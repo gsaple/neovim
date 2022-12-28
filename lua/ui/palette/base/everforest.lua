fad5d55622f1bc8ed35ecaaec8e746936e7b2338
gsaple <yymelau@gmail.com>
2022-12-28 20:48:47 +1100

    more colour schemes

---
+++ lua/ui/palette/base/everforest.lua
@@ -0,0 +1,13 @@
+return {
+  black   = '#272e33',
+  red     = '#e67e80',
+  green   = '#a7c080',
+  yellow  = '#dbbc7f',
+  blue    = '#7fbbb3',
+  magenta = '#d699b6',
+  cyan    = '#83c092',
+  white   = '#d3c6aa',
+  orange  = '#e69875',
+  grey    = '#7a8478',
+  none    = 'NONE',
+}
---
+++ lua/ui/palette/base/kanagawa.lua
@@ -0,0 +1,13 @@
+return {
+  black   = '#16161D',
+  red     = '#FF5D62',
+  green   = '#98BB6C',
+  yellow  = '#E6C384',
+  blue    = '#7E9CD8',
+  magenta = '#D27E99',
+  cyan    = '#68AD99',
+  white   = '#DCD7BA',
+  orange  = '#FFA066',
+  grey    = '#727169',
+  none    = 'NONE',
+}
