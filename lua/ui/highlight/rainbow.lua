a0516396e7888fad9434094c17d5afde9221d0e1
gsaple <yymelau@gmail.com>
2023-06-20 21:52:08 +1000

    rainbow highlight group

--- lua/ui/highlight/rainbow.lua
+++ lua/ui/highlight/rainbow.lua
@@ -1,13 +1,13 @@
 M = {}
 M.highlight = function(C, D)
   return {
-    rainbowcol1 = { bg = C.none , fg = C.orange, bold = true },
-    rainbowcol2 = { bg = C.none , fg = C.blue, bold = true },
-    rainbowcol3 = { bg = C.none , fg = C.magenta, bold = true },
-    rainbowcol4 = { bg = C.none , fg = C.yellow, bold = true },
-    rainbowcol5 = { bg = C.none , fg = C.cyan, bold = true },
-    rainbowcol6 = { bg = C.none , fg = C.red, bold = true },
-    rainbowcol7 = { bg = C.none , fg = C.green, bold = true },
+    TSRainbowOrange = { bg = C.none , fg = C.orange, bold = true },
+    TSRainbowBlue =   { bg = C.none , fg = C.blue, bold = true },
+    TSRainbowViolet = { bg = C.none , fg = C.magenta, bold = true },
+    TSRainbowYellow = { bg = C.none , fg = C.yellow, bold = true },
+    TSRainbowCyan =   { bg = C.none , fg = C.cyan, bold = true },
+    TSRainbowRed =    { bg = C.none , fg = C.red, bold = true },
+    TSRainbowGreen =  { bg = C.none , fg = C.green, bold = true },
   }
 end
 return M
