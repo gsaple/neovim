be2b19c44a43cedfac140821483cf466901d4ec0
gsaple <yymelau@gmail.com>
2023-08-28 12:02:19 +1000

    nvim surround

--- lua/plugins/load.lua
+++ lua/plugins/load.lua
@@ -238,4 +238,15 @@ return {
     end,
   },
   -- }}}
+
+  -- nvim-surround {{{
+  {
+    "kylechui/nvim-surround",
+    event = { "InsertEnter" },
+    version = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  -- }}}
 }
