2a69299edd046af2d215b632bac3a43896957cc8
gsaple <yymelau@gmail.com>
2023-08-28 12:04:16 +1000

    add some css snippets

---
+++ lua/snippets/css.lua
@@ -0,0 +1,33 @@
+return {
+  s("css-reset", fmt([[
+  *,
+  *::before,
+  *::after {{
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }}
+  {}
+  ]], {
+    i(0),
+  })),
+
+  s("center-box", fmt([[
+  margin: {}px auto;
+  {}
+  ]], {
+    i(1, "number"),
+    i(0),
+  })),
+
+  s("center-content", fmt([[
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  {}
+  ]], {
+    i(0),
+  })),
+}
+
+
